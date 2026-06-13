622a3cac5e42ece2208823a75b29fe9912a4b983
Coder-yt <animeupdatesau04@gmail.com>
2026-06-13 14:13:42 +0530

    broadcast.py

---
+++ plugins/broadcast.py
@@ -0,0 +1,80 @@
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+from pyrogram import Client, filters
+
+from config import OWNER_ID
+from database import get_users
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+def owner(_, __, msg):
+
+    return (
+        msg.from_user
+        and msg.from_user.id == OWNER_ID
+    )
+
+
+owner_filter = filters.create(owner)
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+@Client.on_message(
+    filters.command("broadcast")
+    & owner_filter
+)
+async def broadcast(
+    client,
+    message
+):
+
+    if not message.reply_to_message:
+
+        return await message.reply_text(
+            "Reply to a message"
+        )
+
+
+    sent = 0
+    failed = 0
+
+
+    async for user in await get_users():
+
+        try:
+
+            await message.reply_to_message.copy(
+                user["_id"]
+            )
+
+            sent += 1
+
+
+        except:
+
+            failed += 1
+
+
+
+    await message.reply_text(
+        f"""
+✅ Broadcast Done
+
+Sent: {sent}
+Failed: {failed}
+"""
+    )
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
