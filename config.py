de6cd7f5ea096bc77a7eb03befcc02234c5b9cfe
Coder-yt <animeupdatesau04@gmail.com>
2026-06-13 14:00:56 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,98 @@
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+API_ID = int(os.getenv("API_ID", "0"))
+API_HASH = os.getenv("API_HASH")
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+OWNER_ID = 7284759394
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+CHANNEL_ID = -1004055876401
+
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+LOG_CHANNEL = int(
+    os.getenv("LOG_CHANNEL", "0")
+)
+
+
+BOT_USERNAME = "AU_AutoButtonAdder_bot"
+
+
+BUTTONS = [
+    [
+        ("📢 Updates", "https://t.me/Anime_UpdatesAU"),
+        ("🏴‍☠️ One Piece", "https://t.me/+t_RfBWnTbqNhYWVl"),
+        ("✨️ Anime", "https://t.me/AU_Anime_In_Hindi")
+    ]
+]
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+START_TEXT = """
+👋 Welcome
+
+Send me any:
+• Text
+• Photo
+• Video
+• Document
+
+I will post it to the channel with buttons.
+
+🚀 AU Auto Button Bot
+"""
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+ABOUT_TEXT = """
+🤖 Bot: AU Auto Button Bot
+
+Features:
+• Auto posting
+• Button support
+• Admin system
+• Broadcast
+• Statistics
+• Logging
+"""
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+OWNER_TEXT = """
+👤 Owner: (@Mr_Mohammed_29)
+7284759394
+"""
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
